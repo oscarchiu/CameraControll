169525f67b8fb36fcc85b81ab0b6c64ce4fc7778
Oscar Chiu <oscar0529@gmail.com>
2015-05-11 14:27:44 +0800

    Initial Commit

---
+++ CameraControll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraControll
+//
+//  Created by Oscar Chiu on 2015/5/11.
+//  Copyright (c) 2015年 OscarChiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CameraControll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraControll
+//
+//  Created by Oscar Chiu on 2015/5/11.
+//  Copyright (c) 2015年 OscarChiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
